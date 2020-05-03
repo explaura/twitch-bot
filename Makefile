4cf0ead76a58e36bc4139aeb5f7a9f27949955d5
Laura <laurakbrandon@gmail.com>
2020-05-03 16:39:57 +1000

    add basic dependencies and add tmi which listens to twitch messages for a given twitch channel

---
+++ Makefile
@@ -0,0 +1,12 @@
+OAUTH_TOKEN ?= $(shell aws ssm get-parameter --name '/twitch/oauth-token' --with-decryption --query Parameter.Value --output text)
+
+default: lint test
+.PHONY: default
+
+lint:
+	npm run lint
+.PHONY: lint
+
+install:
+	npm install
+.PHONY: install
---
+++ src/index.js
@@ -0,0 +1,40 @@
+const tmi = require("tmi.js");
+
+const OAUTH_TOKEN = process.env.OAUTH_TOKEN;
+const STREAMER_CHANNEL = process.env.STREAMER_CHANNEL;
+const HELP_COMMAND = "!help";
+const SHOTS_COMMAND = "!shots";
+
+const client = new tmi.Client({
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  identity: {
+    username: "simpmasta-3000",
+    password: OAUTH_TOKEN,
+  },
+  channels: [STREAMER_CHANNEL],
+});
+
+client.connect();
+
+client.on("message", (channel, tags, message, self) => {
+  // ignore echoed messages
+  if (self) return;
+
+  if (message.toLowerCase() === HELP_COMMAND) {
+    return client.say(
+      channel,
+      `@${tags.username}, use the command !shots to pour ${STREAMER_CHANNEL} a shot`
+    );
+  } else if (message.toLowerCase() === SHOTS_COMMAND) {
+    return client.say(
+      channel,
+      `@${tags.username}, pouring ${STREAMER_CHANNEL} a shot`
+    );
+  } else {
+    return client.say(channel, `@${tags.username}, new fone who dis?`);
+  }
+});
