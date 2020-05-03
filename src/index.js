855ec7f0da856ccdd352a57b8ab6b895b78c1aee
Laura <laurakbrandon@gmail.com>
2020-05-03 18:25:26 +1000

    add error handling and logging, also use prettier because its pretty

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-const tmi = require("tmi.js");
+const tmi = require('tmi.js');
+const { parse } = require('path');
+const { createLogger } = require('bunyan');
 
 const OAUTH_TOKEN = process.env.OAUTH_TOKEN;
 const STREAMER_CHANNEL = process.env.STREAMER_CHANNEL;
-const HELP_COMMAND = "!help";
-const SHOTS_COMMAND = "!shots";
+const HELP_COMMAND = '!help';
+const SHOTS_COMMAND = '!shots';
+
+const logger = createLogger({
+  name: parse(__filename).name,
+  level: process.env.LOG_LEVEL || 'info',
+});
 
 const client = new tmi.Client({
   options: { debug: true },
@@ -12,7 +19,7 @@ const client = new tmi.Client({
     reconnect: true,
   },
   identity: {
-    username: "simpmasta-3000",
+    username: 'simpmasta-3000',
     password: OAUTH_TOKEN,
   },
   channels: [STREAMER_CHANNEL],
@@ -20,20 +27,34 @@ const client = new tmi.Client({
 
 client.connect();
 
-client.on("message", async (channel, tags, message, self) => {
+client.on('message', async (channel, tags, message, self) => {
   // ignore echoed messages
   if (self) return;
 
   if (message.toLowerCase() === HELP_COMMAND) {
-    return client.say(
-      channel,
-      `@${tags.username}, use the command !shots to pour ${STREAMER_CHANNEL} a shot`
-    );
+    try {
+      client.say(
+        channel,
+        `@${tags.username}, use the command !shots to pour ${STREAMER_CHANNEL} a shot`
+      );
+    } catch (err) {
+      logger.err({
+        err,
+        message: `failed to handle ${HELP_COMMAND} command`,
+      });
+    }
   } else if (message.toLowerCase() === SHOTS_COMMAND) {
-    // TODO: call microcontroller API to kick off pour process
-    return client.say(
-      channel,
-      `@${tags.username}, pouring ${STREAMER_CHANNEL} a shot`
-    );
+    try {
+      // TODO: call microcontroller API to kick off pour process
+      client.say(
+        channel,
+        `@${tags.username}, pouring ${STREAMER_CHANNEL} a shot`
+      );
+    } catch (err) {
+      logger.err({
+        err,
+        message: `failed to handle ${SHOTS_COMMAND} command`,
+      });
+    }
   }
 });
